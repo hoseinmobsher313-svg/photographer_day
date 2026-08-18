abc5f6c00943370808ab033538df79475f1a6e67
hoseinmobsher313-svg <hoseinmobsher313@gmail.com>
2026-08-18 10:50:08 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-const flashToggle = document.getElementById("flashToggle");
-const cameraBox = document.getElementById("cameraBox");
 
-let isOn = false;
-
-flashToggle.addEventListener("click", () => {
-    isOn = !isOn;
-
-    if (isOn) {
-        document.body.classList.add("flash-on");
-        cameraBox.classList.add("visible");
-        flashToggle.textContent = "🔦 خاموش کردن چراغ‌قوه";
-    } else {
-        document.body.classList.remove("flash-on");
-        cameraBox.classList.remove("visible");
-        flashToggle.textContent = "🔦 روشن کردن چراغ‌قوه";
-    }
-});
