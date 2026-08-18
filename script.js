366a631f4914efbf1fccbb63a2b9447e107c0411
hoseinmobsher313-svg <hoseinmobsher313@gmail.com>
2026-08-18 08:59:33 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
 const flashToggle = document.getElementById("flashToggle");
-const cameraBg = document.getElementById("cameraBg");
+const cameraBox = document.getElementById("cameraBox");
 
 let isOn = false;
 
-function updateState() {
-    if (isOn) {
-        document.body.classList.add("flash-on");
-        cameraBg.classList.add("visible");
-        flashToggle.classList.add("on");
-        flashToggle.textContent = "🔔 چراغ‌قوه روشن";
-    } else {
-        document.body.classList.remove("flash-on");
-        cameraBg.classList.remove("visible");
-        flashToggle.classList.remove("on");
-        flashToggle.textContent = "🔔 چراغ‌قوه";
-    }
-}
-
-updateState();
-
 flashToggle.addEventListener("click", () => {
     isOn = !isOn;
-    updateState();
+
+    if (isOn) {
+        document.body.classList.add("flash-on");
+        cameraBox.classList.add("visible");
+        flashToggle.textContent = "🔦 خاموش کردن چراغ‌قوه";
+    } else {
+        document.body.classList.remove("flash-on");
+        cameraBox.classList.remove("visible");
+        flashToggle.textContent = "🔦 روشن کردن چراغ‌قوه";
+    }
 });
